c22e5d409b342e3502ef24bd0e7305133159618e
Jonathan Saindon <jonathan.saindon@gmail.com>
2025-01-13 23:17:39 -0500

    Add entrypoint script and bulb controller class. Works to do rainbow cycle.

---
+++ controller.py
@@ -0,0 +1,26 @@
+import random;
+import time;
+from kasa import iot
+
+class BulbController:
+  _max_hue = 360
+  _max_saturation = 100
+  _max_brightness = 100
+  _step = 10 # How much to change the hue by
+
+  def __init__(self, bulb: iot.IotBulb):
+    self.bulb = bulb
+    self.hue = (30 * (int(random.random() * 12))) % self._max_hue # Random hue between 0 and 330
+
+  async def start_rainbow_cycle(self, change_interval=1000):
+    print(f"[{self.bulb.alias}] Starting rainbow cycle 🌈")
+    print(f"[{self.bulb.alias}] Cycling through colors every", change_interval, "ms")
+    while True:
+      await self._set_next_hue()
+      time.sleep(change_interval / 1000)
+
+  async def _set_next_hue(self):
+    self.hue = (self.hue + self.step) % self._max_hue # Ensure hue stays within the range of 0-359
+    print(f"[{self.bulb.alias}] Updating hue to", self.hue)
+    await self.bulb.set_hsv(self.hue, self._max_saturation, self._max_brightness)
+    await self.bulb.update()
---
+++ main.py
@@ -0,0 +1,39 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from enum import Enum
+from kasa import iot
+from controller import BulbController
+
+load_dotenv()
+ip_addresses = os.getenv("IP_ADDRESSES").split(',')
+
+class Speed(Enum):
+  VerySlow = 5000 # Useful to see the colors change
+  Slow = 3000
+  Medium = 1000
+  Fast = 500
+
+async def get_bulb(ip) -> iot.IotBulb:
+  bulb = iot.IotBulb(ip)
+  await bulb.update() # Load bulb information
+
+  print(f"[{bulb.alias}] {bulb.device_type.name} found")
+  print(f"[{bulb.alias}] Status: {'On' if bulb.is_on else 'Off'}")
+
+  return bulb
+
+async def __main__():
+  bulbControllers = []
+  for ip in ip_addresses:
+    bulb = await get_bulb(ip)
+
+    if not bulb.is_on:
+      print(f"[{bulb.alias}] Turning on")
+      await bulb.turn_on()
+
+    ctrl = BulbController(bulb)
+    bulbControllers.append(ctrl)
+    await ctrl.start_rainbow_cycle(Speed.Fast.value)
+
+asyncio.run(__main__())
